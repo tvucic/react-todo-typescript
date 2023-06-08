f923076d027caf3809102a100d9f92d54bd5dd0a
tvucic <tomo.vucic@gmail.com>
2023-06-08 18:22:01 +0200

    Init commit

---
+++ src/App.js
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+import "./App.css";
+import TodoTable from "./components/TodoTable";
+import NewTodoForm from "./components/NewTodoForm";
+
+function App() {
+  const [showAddTodoForm, setShowAddTodoForm] = useState(false);
+
+  const [todos, setTodos] = useState([
+    { rowNumber: 1, rowDescription: "Todo 1", rowAssigned: "User One" },
+    { rowNumber: 2, rowDescription: "Todo 2", rowAssigned: "User Two" },
+    { rowNumber: 3, rowDescription: "Todo 3", rowAssigned: "User Three" },
+    { rowNumber: 4, rowDescription: "Todo 4", rowAssigned: "User Four" },
+  ]);
+
+  const addTodo = (description, assigned) => {
+    let rowNumber = 0;
+    if (todos.length > 0) {
+      rowNumber = todos[todos.length - 1].rowNumber + 1;
+    } else {
+      rowNumber = 1;
+    }
+    const newTodo = {
+      rowNumber: rowNumber,
+      rowDescription: description,
+      rowAssigned: assigned,
+    };
+    setTodos((todos) => [...todos, newTodo]);
+  };
+
+  const deleteTodo = (deleteTodoRowNumber) => {
+    let filtered = todos.filter(function (value) {
+      return value.rowNumber !== deleteTodoRowNumber;
+    });
+    setTodos(filtered);
+  };
+
+  return (
+    <div className="mt-5 container">
+      <div className="card">
+        <div className="card-header">Your Todo's</div>
+        <div className="card-body">
+          <TodoTable todos={todos} deleteTodo={deleteTodo} />
+          <button
+            onClick={() => setShowAddTodoForm(!showAddTodoForm)}
+            className="btn btn-primary"
+          >
+            {showAddTodoForm ? "Close New Todo" : "New Todo"}
+          </button>
+          {showAddTodoForm && <NewTodoForm addTodo={addTodo} />}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
